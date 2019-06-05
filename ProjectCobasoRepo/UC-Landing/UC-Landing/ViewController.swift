8d1c84ba9a4b0cf6bdf91ef3435080f1de96c775
Amit Tomar <amit.tomar@c3iot.com>
2019-06-05 14:48:36 -0700

    Push UC Landing

---
+++ ProjectCobasoRepo/UC-Landing/UC-Landing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UC-Landing
+//
+//  Created by Amit Tomar on 6/1/19.
+//  Copyright © 2019 com.uc.qdeq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
